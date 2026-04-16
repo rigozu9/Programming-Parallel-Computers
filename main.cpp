94ebdd87f55a5fdea7e744e25c975736d6ec2b40
Riku Nummi <rikunummi9@gmail.com>
2026-04-16 14:32:31 +0300

    Add C++ practice

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
+
+
+int comparison() {
+    int x = 5;
+    double y = 2.5;
+
+    x = y;
+    std::cout << x << std::endl;
+    return 0;
+}
 
 int main() {
-    int x = 5;
-    std::cout << x << std::endl;
+    comparison();
+    try {
+        std::string s = "abc";
+        int x = std::stoi(s);
+    }
+    catch (std::invalid_argument& e) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
