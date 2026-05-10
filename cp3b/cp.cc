944c96cd8633ea5ebb29621be0008dc13456980a
Riku Nummi <rikunummi9@gmail.com>
2026-05-10 20:41:32 +0300

    CP3B done with 5/5

--- cp3b/cp.cc
+++ cp3b/cp.cc
@@ -53,9 +53,9 @@ void correlate(int ny, int nx, const float *data, float *result) {
             normalized[y*no_of_blocks + k] = v;
         }
     }
-    int end_block = ny - (ny % 4);
+    int end_block = ny - (ny % 8);
     #pragma omp parallel for schedule(dynamic, 1)
-    for (int i0 = 0; i0 < end_block; i0 += 4) {
+    for (int i0 = 0; i0 < end_block; i0 += 8) {
         for (int j0 = 0; j0 + 3 < i0; j0 += 4) {
             float8_t sum00 = {0,0,0,0,0,0,0,0};
             float8_t sum01 = {0,0,0,0,0,0,0,0};
@@ -77,11 +77,35 @@ void correlate(int ny, int nx, const float *data, float *result) {
             float8_t sum32 = {0,0,0,0,0,0,0,0};
             float8_t sum33 = {0,0,0,0,0,0,0,0};
 
+            float8_t sum40 = {0,0,0,0,0,0,0,0};
+            float8_t sum41 = {0,0,0,0,0,0,0,0};
+            float8_t sum42 = {0,0,0,0,0,0,0,0};
+            float8_t sum43 = {0,0,0,0,0,0,0,0};
+
+            float8_t sum50 = {0,0,0,0,0,0,0,0};
+            float8_t sum51 = {0,0,0,0,0,0,0,0};
+            float8_t sum52 = {0,0,0,0,0,0,0,0};
+            float8_t sum53 = {0,0,0,0,0,0,0,0};
+
+            float8_t sum60 = {0,0,0,0,0,0,0,0};
+            float8_t sum61 = {0,0,0,0,0,0,0,0};
+            float8_t sum62 = {0,0,0,0,0,0,0,0};
+            float8_t sum63 = {0,0,0,0,0,0,0,0};
+
+            float8_t sum70 = {0,0,0,0,0,0,0,0};
+            float8_t sum71 = {0,0,0,0,0,0,0,0};
+            float8_t sum72 = {0,0,0,0,0,0,0,0};
+            float8_t sum73 = {0,0,0,0,0,0,0,0};
+
             for (int k = 0; k < no_of_blocks; k++) {
                 float8_t a0 = normalized[(i0+0)*no_of_blocks + k];
                 float8_t a1 = normalized[(i0+1)*no_of_blocks + k];
                 float8_t a2 = normalized[(i0+2)*no_of_blocks + k];
                 float8_t a3 = normalized[(i0+3)*no_of_blocks + k];
+                float8_t a4 = normalized[(i0+4)*no_of_blocks + k];
+                float8_t a5 = normalized[(i0+5)*no_of_blocks + k];
+                float8_t a6 = normalized[(i0+6)*no_of_blocks + k];
+                float8_t a7 = normalized[(i0+7)*no_of_blocks + k];
 
                 float8_t b0 = normalized[(j0+0)*no_of_blocks + k];
                 float8_t b1 = normalized[(j0+1)*no_of_blocks + k];
@@ -92,7 +116,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
                 sum01 += a0 * b1;
                 sum02 += a0 * b2;
                 sum03 += a0 * b3;
-                
+
                 sum10 += a1 * b0;
                 sum11 += a1 * b1;
                 sum12 += a1 * b2;
@@ -107,6 +131,26 @@ void correlate(int ny, int nx, const float *data, float *result) {
                 sum31 += a3 * b1;
                 sum32 += a3 * b2;
                 sum33 += a3 * b3;
+
+                sum40 += a4 * b0;
+                sum41 += a4 * b1;
+                sum42 += a4 * b2;
+                sum43 += a4 * b3;
+
+                sum50 += a5 * b0;
+                sum51 += a5 * b1;
+                sum52 += a5 * b2;
+                sum53 += a5 * b3;
+
+                sum60 += a6 * b0;
+                sum61 += a6 * b1;
+                sum62 += a6 * b2;
+                sum63 += a6 * b3;
+
+                sum70 += a7 * b0;
+                sum71 += a7 * b1;
+                sum72 += a7 * b2;
+                sum73 += a7 * b3;
             }
 
             result[(i0+0) + (j0+0)*ny] = sum_lanes(sum00);
@@ -128,10 +172,30 @@ void correlate(int ny, int nx, const float *data, float *result) {
             result[(i0+3) + (j0+1)*ny] = sum_lanes(sum31);
             result[(i0+3) + (j0+2)*ny] = sum_lanes(sum32);
             result[(i0+3) + (j0+3)*ny] = sum_lanes(sum33);
+
+            result[(i0+4) + (j0+0)*ny] = sum_lanes(sum40);
+            result[(i0+4) + (j0+1)*ny] = sum_lanes(sum41);
+            result[(i0+4) + (j0+2)*ny] = sum_lanes(sum42);
+            result[(i0+4) + (j0+3)*ny] = sum_lanes(sum43);
+
+            result[(i0+5) + (j0+0)*ny] = sum_lanes(sum50);
+            result[(i0+5) + (j0+1)*ny] = sum_lanes(sum51);
+            result[(i0+5) + (j0+2)*ny] = sum_lanes(sum52);
+            result[(i0+5) + (j0+3)*ny] = sum_lanes(sum53);
+
+            result[(i0+6) + (j0+0)*ny] = sum_lanes(sum60);
+            result[(i0+6) + (j0+1)*ny] = sum_lanes(sum61);
+            result[(i0+6) + (j0+2)*ny] = sum_lanes(sum62);
+            result[(i0+6) + (j0+3)*ny] = sum_lanes(sum63);
+
+            result[(i0+7) + (j0+0)*ny] = sum_lanes(sum70);
+            result[(i0+7) + (j0+1)*ny] = sum_lanes(sum71);
+            result[(i0+7) + (j0+2)*ny] = sum_lanes(sum72);
+            result[(i0+7) + (j0+3)*ny] = sum_lanes(sum73);
         }
 
         // #pragma omp parallel for schedule(dynamic, 1)
-        for (int ii = i0; ii <= i0 + 3; ii++) {
+        for (int ii = i0; ii <= i0 + 7; ii++) {
             for (int j = i0; j <= ii; j++) {
                 float8_t dot8 = {0,0,0,0,0,0,0,0};
 
